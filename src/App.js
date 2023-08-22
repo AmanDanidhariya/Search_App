982b47ddfb50cc8c52f3b16453093898f8e87255
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-08-22 23:11:22 +0530

    import faker and  createRandom Products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
+import { faker } from "@faker-js/faker";
 
+    function createRandomProduct () {
+	return {
+	    id: faker.datatype.uuid(),
+	    product: faker.commerce.product(),
+	    department: faker.commerce.department(),
+	    price: faker.commerce.price(),
+	    productAdjective: faker.commerce.productAdjective(),
+	    productName: faker.commerce.productName(),
+	    image: faker.image.fashion()
+	}
+    }
+
+    let products5 = faker.helpers.multiple(createRandomProduct, { count: 10, });
+
+    console.log(products5);
 
 function App() {
 
