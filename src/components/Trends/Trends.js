796174d5c87c63188c30f563f71e89fc581b2511
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-08-23 22:19:58 +0530

    add popular suggestions array to trends component

--- src/components/Trends/Trends.js
+++ src/components/Trends/Trends.js
@@ -3,6 +3,13 @@ import style from "./Trends.module.css";
 import TrendsCard from "../TrendsCard/TrendsCard";
 
 const Trends = ({ data }) => {
+  const suggestionList = [
+    "Striped shirt dress",
+    "Satin shirts",
+    "Denim jumpsuit",
+    "Leather dresses",
+    "Solid tshirts",
+  ];
   return (
     <>
       <div className={style.trends_container}>
@@ -10,7 +17,12 @@ const Trends = ({ data }) => {
         <div className={style.trends_card_container}>
           <TrendsCard data={data} />
         </div>
-        {/* <h4 className={style.trends_font}>Popular suggestions</h4> */}
+        <h4 className={style.trends_font}>Popular suggestions</h4>
+        {suggestionList.map((item) => (
+          <div key={item.id} className={style.suggestions}>
+            {item}
+          </div>
+        ))}
       </div>
     </>
   );
