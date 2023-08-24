28708b54b85a641728a7e75dd4837c51f2f62bd0
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-08-24 16:13:23 +0530

    adding liked button to productCard

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./ProductCard.module.css";
+import { FaHeart } from 'react-icons/fa';
 
 const ProductCard = ({ data }) => {
+  //array for stor product_id
+  const [isLiked , setIsLiked] = useState([])
+
+const handleLike = (productId) =>{
+  if(isLiked.includes(productId)){
+    setIsLiked(isLiked.filter(id=> id !== productId));
+  }else{
+    //update the isLiked array 
+    setIsLiked([...isLiked, productId]);
+  }
+}
+
   return (
     <div className={style.productCard_container}>
       {data.map((product) => {
         //discount 10%
         const discountedPrice = product.price - (product.price * 10) / 100;
+        //check if the product liked 
+        const liked = isLiked.includes(product.id);
+
         return (
           <div key={product.id} className={style.product}>
             <div className={style.product_image_container}>
+            <button onClick={()=>handleLike(product.id)}>
+            <FaHeart className={style.heart} color={liked ? 'red' : 'white'}/>
+            </button>
+            
               <img
                 src={product.image}
                 alt={product.productName}
                 className={style.product_image}
               />
+              
               <div className={style.product_overlay}>
+                
                 <button className={style.view_product_button}>
                   View Product
                 </button>
